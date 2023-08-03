f5590bff8415ba3e373de01f3c532a6faa3aa642
Lucas <lucaslimalves10@gmail.com>
2023-08-03 14:30:07 -0300

    terminei a calculadora

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const inputAltura = document.querySelector("#altura");
 
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
+  e.preventDefault(); //impede o formulário de ser enviado
 
   const peso = Number(inputPeso.value);
   const altura = Number(inputAltura.value);
@@ -14,23 +14,60 @@ form.addEventListener("submit", (e) => {
   }
 
   if (!altura) {
-    setResultado("Altura inválido", false);
+    setResultado("Altura inválida", false);
     return;
   }
+
+  const imc = getImc(peso, altura);
+  const classImc = getClassImc(imc);
+
+  const msg = `Seu IMC é ${imc} (${classImc})`;
+
+  setResultado(msg, true);
 });
 
+function getClassImc(imc) {
+  const classImc = [
+    "Abaixo do peso",
+    "Peso normal",
+    "Sobrepeso",
+    "Obesidade grau 1",
+    "Obesidade grau 2",
+    "Obesidade grau 3",
+  ];
+
+  if (imc >= 39.9) return classImc[5];
+  if (imc >= 34.9) return classImc[4];
+  if (imc >= 29.9) return classImc[3];
+  if (imc >= 24.9) return classImc[2];
+  if (imc >= 18.5) return classImc[1];
+  if (imc < 18.5) return classImc[0];
+}
+
+// Função que calcula o IMC
+function getImc(peso, altura) {
+  return (peso / altura ** 2).toFixed(2);
+}
+
+// Função para criar os parágrafos.
 function createP() {
   const p = document.createElement("p");
   return p;
 }
 
-function setResultado(msg) {
+//Função que adiciona o parágrafo a div de resultado com alguma mensagem.
+function setResultado(msg, isValid) {
   const resultado = document.querySelector("#resultado");
 
   resultado.innerHTML = "";
 
   const p = createP();
+
+  if (isValid) {
+    p.classList.add("valid");
+  } else p.classList.add("invalid");
+
   p.innerHTML = msg;
 
-  resultado.appendChild(p);
+  resultado.appendChild(p); //insere o parágrafo criado dentro do resultado
 }
